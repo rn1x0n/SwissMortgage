4a56c5d6e3248c18f38280529d55036df0e2f51e
Richard Nixon <richard2718@gmail.com>
2013-06-09 16:07:50 +0200

    rename SiwssMortgage to pkg

--- scripts/test-code.R
+++ scripts/test-code.R
@@ -1,31 +1,33 @@
 # TEST CODE
 
-rm(list = ls())
-library(ggplot2)
-library(shiny)
-library(xtable)
-source("SwissMortgage/R/interest.R")
-runApp("SwissMortgage/inst/SwissMortgage")
+source("_setup.R")
+
+runApp("pkg/inst/shiny")
 
 
 #####################################################################
 # Function to include in library to start a web server
-# shiny::runApp(system.file('SwissMortgage', package='SwissMortgage'))
+# shiny::runApp(system.file('shiny', package='SwissMortgage'))
 
 # Putting on web
 # http://glimmer.rstudio.com:8787/
 # Git pull the latest version to project SwissMortgage
 # Copy server and ui to ShinyApps
-system("cp /home/m1x0n/Projects/SwissMortgage/SwissMortgage/inst/SwissMortgage/*.* /home/m1x0n/ShinyApps/SwissMortgage/", intern = TRUE)
+system("cp /home/m1x0n/Projects/SwissMortgage/SwissMortgage/inst/shiny/*.* /home/m1x0n/ShinyApps/SwissMortgage/", intern = TRUE)
 
 # External link
 # http://glimmer.rstudio.com/m1x0n/SwissMortgage
+
+# Building windows file
+# http://win-builder.r-project.org/upload.aspx
 #####################################################################
 # Interest payment functions
 
 debt <- 100000; rate <- 1; period <- 3
 
-flexRate <- flex.rate()
+flexRate <- flex.rate(times = c(0, 3, 6, 10), rates = c(1, 2, 2.5, 2), last.time = 12)
+ggplot(flexRate, aes(x = month, y = rate)) + geom_line()
+
 currentFixRates <- fix.rate(period = 1:10)
 fix.rate(start.time = 0, period = 5, current.fix.rates = currentFixRates, flex.rate = flexRate)
 
@@ -48,7 +50,7 @@ shinyPlan <- list(
 plan <- shinyPlan2plan(
   shinyPlan = shinyPlan,
   currentFixRates = fix.rate(period = 1:10),
-  flexRate = flex.rate()
+  flexRate = flexRate
   )
 
 
